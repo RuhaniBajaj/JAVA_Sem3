ad4e585c8787e029a45085c365ea7e7fe5f6f5b1
Ruhani Bajaj <bajaj.ruhani1404@gmail.com>
2025-09-17 12:22:00 +0530

    Pattern of reverse triangle

---
+++ Patterns.java/reversetriangle.java
@@ -0,0 +1,17 @@
+public class reversetriangle {
+     public static void main(String[] args){
+        for (int i=8; i>=1; i--){
+            for (int j=1;j<=i;j++){
+                 System.out.print(j);
+            }
+            System.out.println() ;
+        }
+
+         for (int i=2; i<8; i++){
+            for (int j=1;j<=i;j++){
+                 System.out.print(j);
+            }
+            System.out.println() ;
+        }
+     }
+}
